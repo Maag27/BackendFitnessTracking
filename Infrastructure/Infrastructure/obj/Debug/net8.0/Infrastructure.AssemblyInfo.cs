a5cd8d6c5b4bf0716b308315003a2f4b9ee17daf
NovaPage <novapagecompany@gmail.com>
2024-11-08 18:47:42 -0500

    Se establecio los parametros de metricas

--- Infrastructure/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db714af77b751105bb16765f91960d35faf5e12d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c2d53bbeafea6b3823702d3ae7b394d502d3b8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
