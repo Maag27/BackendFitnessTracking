5a13d6d38dcbbd57b0f83034b758110e8abad034
NovaPage <novapagecompany@gmail.com>
2024-11-11 19:57:37 -0500

    se permiten solicitudes desde cualquier lugar

--- Web/Program.cs
+++ Web/Program.cs
@@ -60,7 +60,7 @@ namespace ApiSampleFinal
             {
                 options.AddPolicy("AllowAngularApp", policy =>
                 {
-                    policy.WithOrigins("http://localhost:4200")
+                        policy.AllowAnyOrigin()
                           .AllowAnyMethod()
                           .AllowAnyHeader();
                 });
